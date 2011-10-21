7fc3515b0fcd4f83b3b8a036581fca214c43fe56
dave@kapoq.com <dave@kapoq.com>
2011-10-21 15:22:56 +0200

    Create /gem directory if necessary

--- lib/rubygems-source.rb
+++ lib/rubygems-source.rb
@@ -12,7 +12,7 @@ module Rubygems
       set :raise_errors, true
 
       helpers do
-        def path_to_gem(gem_file_name)
+        def path_to_gem(gem_file_name = "")
           File.join(settings.public_folder, "gems", gem_file_name)
         end
         
@@ -37,6 +37,7 @@ module Rubygems
         body = StringIO.new(request.body.read)
         if valid_gem_package?(body)
           clear_gem_specs_and_update_gem_indices
+          FileUtils.mkdir_p(path_to_gem) unless File.directory?(path_to_gem)
         else
           status 403
         end
